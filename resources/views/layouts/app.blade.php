9c1470af1f1f96b7b99fb0b921fc5935f07ea71f
maumans <mansaremaurice100@gmail.com>
2021-01-11 11:27:35 +0000

    build_wecome_page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,14 +19,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    @yield("css")
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light shadow-sm fixed-top" style="background-color: #ff5050;color: white">
             <div class="container">
-                <a class="navbar-brand text-light" href="{{ url('/') }}">
-                    ColiSmarT
+                <a class="navbar-brand text-light font-weight-bold" href="{{ url('/') }}">
+                    <h2 class="my-auto">ColiSmarT</h2>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,31 +33,42 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav mr-auto ml-3 my-auto">
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="{{ url('/home') }}">{{ __(' Accueil') }}</a>
+                            <a class="nav-link text-light font-weight-bold" href="{{ url('/home') }}">Envie d'enyoyer un colis</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light font-weight-bold" href="{{ url('/home') }}">Envie de transporter un colis</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light font-weight-bold" href="{{ url('/home') }}">Comment ça marche ?</a>
                         </li>
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                           <div class="p-1 rounded-pill bg-dark">
+                               <a class="nav-link text-light font-weight-bold" href="{{ url('/home') }}">{{ __(' Accueil') }}</a>
+                           </div>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link text-light font-weight-bold" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link text-light font-weight-bold" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle font-weight-bold" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,56 @@
 @extends("layouts/app")
 
-@section("css")
-    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-@endsection
-
 @section("content")
 
     <body style="background:url({{asset("img/welcomeBg.jpg")}}) no-repeat;background-size: auto; ">
         <div id="welcomeDivPrincipale">
-           <div class="text-light d-md-flex py-5 container">
-               <div class="flex-grow-1 mr-5" id="mau">
-                   <img src="{{asset("img/welcomeLivreur.png")}}" class="w-75" alt="">
-               </div>
-               <div class="flex-grow-1">
-                   <div class="mb-4">
-                       <h1 class="smart">
-                           Envoi de colis de façon Smart
-                           <div style="height: 4px;width: 50%;background-color: white;margin-bottom: 10px"></div>
-                           <div style="height: 4px;width: 45%;background-color: white; margin-bottom: 10px"></div>
-                           <div style="height: 4px;width: 40%;background-color: white; margin-bottom: 10px"></div>
-                           <div style="height: 4px;width: 35%;background-color: white; margin-bottom: 10px"></div>
-                       </h1>
-
+           <div class="container py-5">
+               <div class="text-light row">
+                   <div class="col-6">
+                       <img src="{{asset("img/welcomeLivreur.png")}}" class="w-100" alt="">
                    </div>
-                   <div>
-                       <form action="" class="form-group welcome-form">
-                           <h3>
-                               <div>
-                                   Je veux envoyer un colis de
+                   <div class="col-6">
+                       <div class="mb-4">
+                           <h1 class="smart">
+                               Envoi de colis de façon Smart
+                               <div class="row">
+                                   <div class="col-5 col-md-2">
+                                        <div class="rounded-circle" style="height: 50px;width: 50px;border: 5px solid white">
+                                            <div class="rounded-circle m-auto" style="height: 30px;width: 30px;border: 5px solid white">
+                                                <div class="rounded-circle m-auto" style="height: 15px;width: 15px;border: 5px solid white">
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                   </div>
+                                   <div class="col-7 col-md-10">
+                                       <div style="height: 4px;width: 100%;background-color: white;margin-bottom: 10px"></div>
+                                       <div style="height: 4px;width: 80%;background-color: white; margin-bottom: 10px"></div>
+                                       <div style="height: 4px;width: 60%;background-color: white; margin-bottom: 10px"></div>
+                                       <div style="height: 4px;width: 40%;background-color: white; margin-bottom: 10px"></div>
+                                   </div>
                                </div>
-                               <div>
-                                   <input type="text">
-                                   à
-                                   <input type="text">
-                               </div>
-                              <div>
-                                  à partir du
-                                  <input type="text">
-                              </div>
-                           </h3>
-                       </form>
+                           </h1>
+
+                       </div>
+                       <div>
+                           <form action="" class="form-group welcome-form">
+                               <h3>
+                                   <div>
+                                       Je veux envoyer un colis de
+                                   </div>
+                                   <div>
+                                       <input type="text">
+                                       à
+                                       <input type="text">
+                                   </div>
+                                   <div>
+                                       à partir du
+                                       <input type="text">
+                                   </div>
+                               </h3>
+                           </form>
+                       </div>
                    </div>
                </div>
            </div>
